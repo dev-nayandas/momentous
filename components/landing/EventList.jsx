3a5d6da610c38eb3fbb7c1bb310f23bfa2e3130b
dev-nayandas <rich.nayan@gmail.com>
2025-02-10 01:08:45 +0600

    database connection issue solved

--- components/landing/EventList.jsx
+++ components/landing/EventList.jsx
@@ -4,6 +4,8 @@ import EventCard from "./EventCard";
 const EventList = async ({query}) => {
   const allMoments = await getAllMoments(query);
 
+  console.log(allMoments);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
       {allMoments.map((moment) => (
--- services/mongo.js
+++ services/mongo.js
@@ -1,11 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
+const MONGO_URI = process.env.MONGO_URI
+const cached = {}
 export async function dbConnect() {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected");
-        return conn;
-    } catch(err){
-        console.log(err);
+  if (!MONGO_URI) {
+    throw new Error(
+      "Please define the MONGO_URI environment variable inside .env.local"
+    )
+  }
+  if (cached.connection) {
+    return cached.connection
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false
     }
+    cached.promise = mongoose.connect(MONGO_URI, opts)
+  }
+  try {
+    cached.connection = await cached.promise
+  } catch (e) {
+    cached.promise = undefined
+    throw e
+  }
+  return cached.connection
 }
